cabf85c9c832b3b26fd7f0da074baf79b19cc103
Saqer <ahmadsaqer95@outloo.com>
2021-03-23 18:34:44 +0100

    add functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
 
 //components
 import Todos from './components/Todos';
+import TodoList from './components/TodoList';
+import InputArea from './components/InputArea'
 
 function App() {
+  const [todos , setTodos] = useState([]);
+  const [textInput, setTextInput] = useState(""); 
   return (
     <div className="App">
-
-      <Todos />
+      <h1> </h1>
+      <InputArea todos={todos} setTodos={setTodos} setTextInput={setTextInput} />
+      <TodoList todos={todos} />
+      
 
     </div>
   );
