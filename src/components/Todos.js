48f079274ec4527fc0c09940dabf54cd706527e5
Saqer <ahmadsaqer95@outloo.com>
2021-03-23 16:11:11 +0100

    add Todos

---
+++ src/components/Todos.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//css
+import './Todos.css';
+const Todos = () => {
+    return (
+        <div className="item" >
+            <div className="container" ></div>
+            <button>done</button>
+            <button>delete</button>
+            <button>edit</button>
+             </div>
+
+    );
+}
+
+export default Todos;
